61026e1839b8c8ccd5e6cc6a301833a76d228edc
Binit Datta <binitdatta@gmail.com>
2017-07-30 09:48:34 -0500

    class level annotations

--- src/main/java/com/rollingstone/SpringBootMicroserviceStarterTemplateTODOChangeThisToYourApp.java
+++ src/main/java/com/rollingstone/SpringBootMicroserviceStarterTemplateTODOChangeThisToYourApp.java
@@ -4,9 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration // Sprint Boot Auto Configuration
+@SpringBootApplication
+@SuppressWarnings("deprecation")
+@EnableAutoConfiguration // Sprint Boot Automatic Configuration
 @ComponentScan(basePackages = "com.rollingstone")
-
+@EnableJpaRepositories("com.rollingstone.dao.jpa") // To segregate MongoDB and JPA repositories. Otherwise not needed.
+@EnableSwagger2
+@EnableDiscoveryClient
+@EnableFeignClients
 public class SpringBootMicroserviceStarterTemplateTODOChangeThisToYourApp {
 
 	public static void main(String[] args) {
